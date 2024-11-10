7c12ef91fee86791ef28e4185599b1f82b8a67de
amano <tk529.com@gmail.com>
2024-11-10 14:10:46 +0900

    kadai_018を修正

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -2,13 +2,19 @@ package kadai_018;
 
 public class KatoExec_Chapter18 {
 
-    public static void main(String[] args) {
-        Kato_Chapter18 KatoTaro = new KatoTaro_Chapter18();
-        Kato_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
-        Kato_Chapter18 KatoHanako = new KatoHanako_Chapter18();
-
-        KatoTaro.execIntroduce();
-        KatoIchiro.execIntroduce();
-        KatoHanako.execIntroduce();
-    }
+	public static void main(String[] args) {
+		
+		Kato_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+		Kato_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+		Kato_Chapter18 KatoHanako = new KatoHanako_Chapter18();
+	
+		KatoTaro.execIntroduce();
+		System.out.println(); 
+		
+		KatoIchiro.execIntroduce();
+		System.out.println();
+		
+		KatoHanako.execIntroduce();
+		System.out.println();
+	}
 }
